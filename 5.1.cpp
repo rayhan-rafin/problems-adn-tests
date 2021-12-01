601916619c6d2d1ebbdabf5a2066eee273ae3b5d
rayhan-rafin <92117578+rayhan-rafin@users.noreply.github.com>
2021-12-01 15:47:07 +0600

    Add files via upload

---
+++ 5.1.cpp
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+    double a1,a2,b1,b2,c1,c2;
+    // goal is to scan a equation, example: 3x+2y +5
+    // where 3,2,5 or other values are scanned
+    printf ("Enter first equation : ");
+    scanf ("%lf",&a1);
+    printf ("x + ");
+    scanf ("%lf",&a1);
+    printf ("y = ");
+    scanf ("%lf\n",&a1);
+}
